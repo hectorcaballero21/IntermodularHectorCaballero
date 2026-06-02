41f2617a9119420211ea04afa8ecc912a72f4fdd
hectorcaballero21 <hectorlourri21@gmail.com>
2026-06-02 14:55:05 +0200

    Api.js en render #2

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios'
 
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  (import.meta.env.DEV
+    ? 'http://localhost:8080'
+    : 'https://intermodularhectorcaballeroback.onrender.com')
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 export default api
